b4a4ee50d7b4589f0f5630e3ee779e659ba553d2
DaniA <daniad01@icloud.com>
2026-02-08 13:07:34 +0200

    Q4 HW

---
+++ Q4.py
@@ -0,0 +1,35 @@
+
+#Last_name: str = str(input("Enter Last name: "))
+#First_name: str = str(input("Enter Last name: "))
+#Country: str = str(input("Enter Last name: "))
+#City: str = str(input("Enter Last name: "))
+#Zipcode: str = str(input("Enter Last name: "))
+
+while True:
+    Last_name = input("Enter Last name: ")
+    if Last_name.isupper():
+        break
+    else:
+        print("Please enter a valid last name")
+
+while True:
+    First_name = input("Enter first name: ")
+    if First_name.islower():
+        break
+    else:
+        print("Please enter a valid first name")
+
+while True:
+    Country = input("Enter Country name: ")
+    if Country.isalpha() and len(Country) <= 3:
+        break
+    else:
+        print("Please enter a valid Country name (ex. US,IL,RU,GER)")
+
+while True:
+    Zipcode = input("Enter Zipcode: ")
+    if Zipcode.isdigit() and len(Zipcode) >= 4:
+        break
+    else:
+        print("Please enter a valid Zipcode (at least 4 numbers)")
+print("Last name: " , Last_name.upper() , "\n" ,"First name: ",First_name.lower(), "\n" ,"country: ",Country, "\n", "zipcode: ", Zipcode)
